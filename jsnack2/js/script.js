dad5812bc4e9691a49562f275707e3af84c0e384
acolombo09 <acolombo0911@gmail.com>
2023-07-06 17:27:33 +0200

    complicandomi_la_vita

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,45 +1,23 @@
 /*
 Snack2
-Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari.
+Crea un array di numeri interi e fai la somma di tutti gli elementi 
+che sono in posizione dispari.
 */
 
-// setto la lista mail con accesso e testo l'array.
-const mailList = ["miamail@gmail.com", "suamail@gmail.com", "nostramail@gmail.com", "vostramaik@gmail.com"];
-// console.log(mailList);
-// console.log(mailList[0]);
-// console.log(mailList[1]);
-// console.log(mailList[2]);
+// creo una costante che mi aiuta ad impostare un numero massimo di numeri da stampare.
+const maxNumbers = [parseInt(100)];
+let number = Math.floor(Math.random() * maxNumbers);
 
-// chiedo all'utente la sua mail.
-const btnLogin = document.querySelector(".request");
-const emailInputElement = document.querySelector("[name='email']");
-let emailCheck = false;
+// // creo la lista dei numeri pari.
+for (let i = 0; i < maxNumbers.length; i++) {
+  console.log(number);
+};
 
-btnLogin.addEventListener("click", function() {
-  if (emailInputElement) {
-    const email = emailInputElement.value;
-    console.log(email);
-  } else {
-    console.log("Impossibile trovare l'elemento con il selettore '[name=mail]'");
-  }
 
-    // controllo e confronto se la mail dell'utente è presente nella lista.
-    const email = emailInputElement.value;
 
-    for (let i = 0; i < mailList.length; i++) {
-    /*creo una variabile che di base è false e diventa true se la mail
-      inserita corrisponde ad una delle mail in elenco
-    */
-    
-    if (email === mailList[i]) {
-      emailCheck = true;
-    }
-  }
-    // stampo alert in base all'esito del controllo.
-    if (emailCheck === true) {
-    alert("Accesso consentito, benvenuto.");
-  }
-    else {
-    alert("Accesso non consentito.");
-  }
-});
+if(number % 2==0){
+  console.log("The number is even");
+}
+else {
+  console.log("The number is odd");
+}
